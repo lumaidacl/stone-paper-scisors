243330dfccb15c886ce1cdca4479fa721c9c634e
Luz Maria Maida <lu@luzs-mbp.lan>
2020-05-18 19:32:56 -0500

    Game code functionality structure

---
+++ game.js
@@ -0,0 +1,48 @@
+(()=>{
+  console.log("this is the game");
+
+  let buttons = document.querySelectorAll(".button");
+  let user = document.querySelector("#selections .user");
+  let computer = document.querySelector("#selections .computer");
+
+  let type_selected = null;
+  let game_options = ['paper', 'stone', 'scissors'];
+  let score = { won: 0, tie: 0, lose: 0};
+
+  const game_logic = {
+    'paper': ['stone'],
+    'stone': ['scissors'],
+    'scissors': ['paper']
+  };
+
+  const whoWon = function(player1, player2){
+    if(player1 === player2){
+      return'tie';
+    }
+    return game_logic[player1].includes(player2) ? 'won': 'lose';
+  }
+
+
+
+  const computerOption = function(){
+    let computer_selection = game_options[Math.floor(Math.random() * game_options.length)];
+    computer.innerHTML = computer_selection;
+    return computer_selection;
+  }
+
+  const scoreAccumulate = function(value){
+    score[value]++;
+    document.querySelector(`#score .${value}`).innerHTML = score[value];
+  }
+
+  const selectOption = function(event){
+    let user_selection = this.dataset.type;
+    user.innerHTML = user_selection;
+    let computer_selection = computerOption();
+    scoreAccumulate( whoWon(user_selection, computer_selection));
+  };
+
+  for (let index=0; index<buttons.length; index++){
+    buttons[index].addEventListener("click", selectOption);
+  }
+})();
