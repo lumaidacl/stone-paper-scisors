cd1f381e819c7b70e4dc725e653b9f9803b04b58
Luz Maria <lu.maidacl@gmail.com>
2020-05-21 20:08:53 -0500

    Building gaming elements

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 (()=>{
-  console.log('this is the game');
+  console.log('This is the game');
 
   let buttons = document.querySelectorAll(".button");
   let user = document.querySelector("#selections .user");
@@ -13,8 +13,14 @@
     'stone': ['scissors', 'lizzard'],
     'scissors': ['paper', 'lizzard'],
     'lizzard': ['spock', 'paper'],
-    'spock': ['scissors', 'rock'],
+    'spock': ['scissors', 'stone'],
   };
+
+  let game_options = Object.keys(game_logic);
+  let center_x = window.innerWidth/2 - 45;
+  let center_y = window.innerHeight/2 + 40;
+  let radius = 150;
+  let angle = 72 * Math.PI / 180;
 
   const whoWon = function(player1, player2){
     if(player1 === player2){
@@ -24,7 +30,6 @@
   }
 
   const computerOption = function(){
-    let game_options = Object.keys(game_logic);
     let computer_selection = game_options[Math.floor(Math.random() * game_options.length)];
     computer.innerHTML = computer_selection;
     return computer_selection;
@@ -42,18 +47,24 @@
     scoreAccumulate( whoWon(user_selection, computer_selection));
   };
 
+  const createButton = function (index) {
+    let button = document.createElement("button");
+    button.classList.add("button");
+    button.textContent = game_options[index];
+
+    button.style.left = `${radius * Math.cos(index*angle) + center_x}px`;
+    button.style.top = `${radius * Math.sin(index*angle) + center_y}px`;
+
+    button.setAttribute("data-type", game_options[index]);
+    button.addEventListener("click", selectOption);
+    gameboard.appendChild(button);
+
+  }
+
   const initGame = function () {
     console.log("Starting game");
-    let game_options = Object.keys(game_logic);
-
     for (let index = 0; index < game_options.length; index ++){
-      console.log("xD");
-      let button = document.createElement("button");
-      button.classList.add("button");
-      button.textContent = game_options[index];
-      button.setAttribute("data-type", game_options[index]);
-      button.addEventListener("click", selectOption);
-      gameboard.appendChild(button);
+      createButton(index);
     }
   }
   initGame();
